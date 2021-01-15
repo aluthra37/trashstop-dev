bdb99fbdf79a3a16b030c164fa441d783a41cee6
Ayush Luthra <aluthra37>
2021-01-14 19:10:25 -0500

    Added support for multiple datasets and plt upgrades

--- build-v2.py
+++ build-v2.py
@@ -12,36 +12,52 @@ import matplotlib.pyplot as plt
 argvs = sys.argv
 argc = len(argvs)
 epochs = int(argvs[1])
+select = argvs[2]
 
-size = 150
-batch_size = 16 #was 64, 32
+size = 128
+batch_size = 32 #was 64, 32
+validation_split = 0.3
 
-dir_path = './classification/garbage-classification/garbage-classification'
-#dir_path = './data-condensed/dataset/train'
-
-num_classes = 7
+if(select=='garbage'):
+  dir_path = './classification/garbage-classification/garbage-classification'
+  num_images_train = 2927-(2927*validation_split)
+  num_images_val = 2927*validation_split
+  num_classes = 7
+  print("Garbage Dataset Selected")
+elif(select=='cifar'):
+  dir_path = './data/dataset/train'
+  num_images_train = 50000-(50000*validation_split)
+  num_images_val = 50000*validation_split
+  num_classes = 10
+  print("CIFAR-10 Dataset Selected")
+elif(select=='cifar-c'):
+  dir_path = './data-condensed/dataset/train'
+  num_images_train = 4010-(4010*validation_split)
+  num_images_val = 4010*validation_split
+  num_classes = 10
+  print("CIFAR-10 Condensed Dataset Selected")
 
 img_list = glob.glob(os.path.join(dir_path, '*/*.jpg'))
-
-print(len(img_list))
+num_images = len(img_list)
+print(num_images)
 
 train=ImageDataGenerator(horizontal_flip=True,
                          vertical_flip=True,
-                         validation_split=0.1,
+                         validation_split=validation_split,
                          rescale=1./255,
-                         shear_range = 0.1,
-                         zoom_range = 0.1,
-                         width_shift_range = 0.1,
-                         height_shift_range = 0.1,)
+                         shear_range = 0.2,
+                         zoom_range = 0.2,
+                         )
 
 test=ImageDataGenerator(rescale=1/255,
-                        validation_split=0.1)
+                        validation_split=validation_split)
 
 train_generator=train.flow_from_directory(dir_path,
                                           target_size=(size,size),
                                           batch_size=batch_size,
                                           class_mode='categorical',
                                           subset='training')
+
 
 test_generator=test.flow_from_directory(dir_path,
                                         target_size=(size,size),
@@ -50,20 +66,13 @@ test_generator=test.flow_from_directory(dir_path,
                                         subset='validation')
 
 labels = (train_generator.class_indices)
-print(labels)
-
 labels = dict((v,k) for k,v in labels.items())
-print(labels)
-
+#print(labels)
 for image_batch, label_batch in train_generator:
   break
 image_batch.shape, label_batch.shape
-print(image_batch.shape, label_batch.shape)
-
-print (train_generator.class_indices)
-
+#print(image_batch.shape, label_batch.shape)
 Labels = '\n'.join(sorted(train_generator.class_indices.keys()))
-
 with open('labels.txt', 'w') as f:
   f.write(Labels)
 
@@ -107,7 +116,7 @@ model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(512,activation='relu'))
 model.add(Dense(128,activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.3))
 model.add(Dense(num_classes,activation='softmax'))
 
 filepath="trained_model.h5"
@@ -124,23 +133,24 @@ model.compile(loss='categorical_crossentropy',
 
 history = model.fit(train_generator,
                               epochs=epochs,
-                              steps_per_epoch=2927//batch_size,
+                              steps_per_epoch=num_images_train//batch_size,
                               validation_data=test_generator,
-                              validation_steps=291//batch_size,
+                              validation_steps=num_images_val//batch_size,
                               workers = 4,
                               callbacks=callbacks_list)
 #41 epoch - 75% #73- 76.9%
 #78 epoch - 80%
 
+plt.figure()
+plt.subplot(211)
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
 # summarize history for loss
+plt.subplot(212)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
